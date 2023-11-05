a62af7d10b1781570917b6c965c43364e74fca55
nev <nev@neverix.io>
2023-11-05 21:55:03 +0000

    :(

--- llama2_model.py
+++ llama2_model.py
@@ -1,5 +1,53 @@
+import equinox as eqx
+import jax
 
 
+class Embedding(eqx.Module):
+    weight: jax.Array
+    
+    vocab_size: int = 32_000
+    hidden_size: int
+    is_unembed: bool
+    
+    def __init__(self, hidden_size, key, is_unembed=False):
+        self.hidden_size = hidden_size
+        self.is_unembed = is_unembed
+        self.weight = (jax.random.normal(
+            key,
+            (self.vocab_size, self.hidden_size)[::(-1 if is_unembed else 1)])
+                       * ((1 / self.hidden_size) ** 0.5))
+    
+    def __call__(self, x):
+        if self.is_unembed:
+            return x @ self.weight
+        else:
+            return self.weight[x]
 
-class LLaMAModel():
-    pass
+
+class LLaMAModel(eqx.Module):
+    embed_tokens: Embedding
+    hidden_size: int
+    
+    def __init__(self, hidden_size, key):
+        self.hidden_size = hidden_size
+        embed_key, key = jax.random.split(key)
+        self.embed_tokens = Embedding(self.hidden_size, embed_key)
+    
+    def __call__(self, x):
+        x = self.embed_tokens(x)
+        return x
+
+
+class LLaMA(eqx.Module):
+    lm_head: Embedding
+    model: LLaMAModel
+    
+    hidden_size: int = 4096
+    
+    def __init__(self, key):
+        lm_head_key, key = jax.random.split(key)
+        self.lm_head = Embedding(self.hidden_size, key=lm_head_key, is_unembed=True)
+        self.model = LLaMAModel(self.hidden_size, key)
+    
+    def __call__(self, x):
+        return self.lm_head(self.model(x))
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from tokenizer import Tokenizer
+from safetensors import safe_open
+from llama2_model import LLaMA
+import jax.numpy as jnp
+import jax
 
 
 tokenizer = Tokenizer("models/Llama-2-7b-hf/tokenizer.model")
-print(tokenizer.encode("Hello world!", bos=True, eos=False))
+input_ids = tokenizer.encode("Hello world!", bos=True, eos=False)
+print(input_ids)
+llama = LLaMA(jax.random.PRNGKey(32))
+print(llama)
+print(llama(jnp.asarray(input_ids)[None, :]))
+
+with safe_open("/home/neverix/micrlhf/models/Llama-2-7b-hf/model-00001-of-00002.safetensors",
+               framework="numpy", device="cpu") as f:
+    print(f.keys())
