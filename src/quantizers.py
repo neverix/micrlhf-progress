2e5e8e06da8d3e92d4b01618b10f69866d04e07b
nev <sshabalin3@gatech.edu>
2024-04-27 00:21:24 +0000

    Reorganize eager dequantization

--- src/quantizers.py
+++ src/quantizers.py
@@ -20,15 +20,21 @@ def make_linear(old_linear: pz.nn.Linear,
                 ) -> pz.nn.Linear:
     param = old_linear.select().at_instances_of(pz.nn.UninitializedParameter).get()
     tensor_data, do_transpose = make_param(
-        param, quant_type, tensor_data, shape, mesh, axis_name_to_mesh_name, return_metadata=True)
-    if quant_type == "q8_0" and do_transpose:
-        new_data = []
-        for d in tensor_data:
-            d = d.reshape(*old_linear.output_axes.values(),
-                          *list(old_linear.input_axes.values())[:-1], -1, d.shape[-1])
-            d = jax.device_put(d)  # TODO replicate
-            d = pz.nx.wrap(d, *old_linear.output_axes.keys(), *old_linear.input_axes.keys(), "quant_group")
-            new_data.append(d)
+    param, quant_type, tensor_data, shape, mesh, axis_name_to_mesh_name, return_metadata=True)
+    if not do_transpose or quant_type == "fp32":
+        # fall back on dequantizing the parameter on HBM
+        param = make_param(param, quant_type, tensor_data, shape, mesh, axis_name_to_mesh_name)
+        return old_linear.select().at_instances_of(pz.nn.Parameter).apply(lambda p: param)
+    new_data = []
+    for d in tensor_data:
+        d = d.reshape(*old_linear.output_axes.values(),
+                        *list(old_linear.input_axes.values())[:-1], -1, d.shape[-1])
+        d = jax.device_put(d)  # TODO replicate
+        d = pz.nx.wrap(d, *old_linear.output_axes.keys(), *old_linear.input_axes.keys(), "quant_group")
+        axes = (*old_linear.input_axes.keys(), "quant_group", *old_linear.output_axes.keys())
+        d = d.untag(*axes).tag(*axes)
+        new_data.append(d)
+    if quant_type == "q8_0":
         return Linear8bitTranspose(
             in_features=old_linear.input_axes,
             out_features=old_linear.output_axes,
@@ -36,9 +42,8 @@ def make_linear(old_linear: pz.nn.Linear,
             quants=new_data[1],
             dtype=param.value_structure.dtype
         )
-    # fall back on dequantizing the parameter on HBM
-    param = make_param(param, quant_type, tensor_data, shape, mesh, axis_name_to_mesh_name)
-    return old_linear.select().at_instances_of(pz.nn.Parameter).apply(lambda p: param)
+    else:
+        raise NotImplementedError(f"Quantization type {quant_type} not implemented")
 
 
 @pz.pytree_dataclass
@@ -119,66 +124,19 @@ def make_param(uninitialized_param: pz.nn.UninitializedParameter,
 
     if quant_type == "fp32":
         dequantized = tensor_data[0]
-
-        dequantized = dequantized.reshape(shape[::-1])
-        if do_transpose:
-            dequantized = dequantized.T  # for jax
-        dequantized = dequantized.astype(dtype).reshape(*named_shape.values())
-        # TODO replicate
-        dequantized = jax.device_put(dequantized)
-        dequantized = pz.nx.wrap(dequantized, *named_shape.keys())
-        return pz.nn.Parameter(
-            dequantized,
-            name,
-        )
     elif quant_type == "q8_0":
-        return Int8Parameter.with_init(
-            name=name,
-            value=None,
-            value_structure=uninitialized_param.value_structure,
-            scale=jax.device_put(tensor_data[0]),
-            quants=jax.device_put(tensor_data[1]),
-            shape=shape[::-1],
-            transpose=do_transpose
-        )
+        dequantized = tensor_data[0].astype(dtype) * tensor_data[1]
     else:
         raise NotImplementedError(f"Quantization type {quant_type} not implemented")
 
-
-# not actually a parameter - doesn't inherit from pz.nn.Parameter
-@pz.pytree_dataclass
-class QuantizedParameter(pz.Struct):
-    name: str = dataclasses.field(metadata={"pytree_node": False})
-    value: Optional[pz.nx.NamedArray]
-    value_structure: pz.chk.ArraySpec
-    shape: Tuple[int] = dataclasses.field(metadata={"pytree_node": False})
-    transpose: bool = dataclasses.field(metadata={"pytree_node": False})
-    
-    def dequantize(self):
-        raise NotImplementedError("Abstract quantized parameter doesn't have a dequantize method")
-
-    @classmethod
-    def with_init(cls, *args, **kwargs):
-        inst_base = cls(*args, **kwargs)
-        return dataclasses.replace(inst_base, value=inst_base.get_value())
-    
-    def get_value(self):
-        dequantized = self.dequantize()
-        dequantized = dequantized.reshape(self.shape)
-        if self.transpose:
-            dequantized = dequantized.T
-        named_shape = self.value_structure.named_shape
-        dtype = self.value_structure.dtype
-        dequantized = jnp.asarray(dequantized.astype(dtype)).reshape(named_shape.values())
-        dequantized = pz.nx.NamedArray(OrderedDict(named_shape), dequantized)
-        return dequantized
-
-
-@pz.pytree_dataclass(has_implicitly_inherited_fields=True)
-class Int8Parameter(QuantizedParameter):
-    scale: Float16[Array, "blocks 1"]
-    quants: Int8[Array, "blocks 32"]
-
-    def dequantize(self):
-        dtype = self.value_structure.dtype
-        return self.scale.astype(dtype) * self.quants
+    dequantized = dequantized.reshape(shape[::-1])
+    if do_transpose:
+        dequantized = dequantized.T  # for jax
+    dequantized = dequantized.astype(dtype).reshape(*named_shape.values())
+    # TODO replicate
+    dequantized = jax.device_put(dequantized)
+    dequantized = pz.nx.wrap(dequantized, *named_shape.keys())
+    return pz.nn.Parameter(
+        dequantized,
+        name,
+    )
