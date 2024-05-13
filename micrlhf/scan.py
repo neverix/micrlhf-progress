d05d6f90f6da0510e189054acf4207671b98767a
nev <sshabalin3@gatech.edu>
2024-05-13 21:44:37 +0000

    Move scan to micrlhf

---
+++ micrlhf/scan.py
@@ -0,0 +1,24 @@
+from penzai import pz
+import jax
+
+
+@pz.pytree_dataclass
+class ScanSequential(pz.Layer):
+    layer: pz.Layer
+
+    # @jax.jit
+    def __call__(self, inputs):
+        return jax.lax.scan(
+            lambda h, l: (l(h), None),
+            inputs,
+            jax.tree_map(lambda x: x.untag("layer"), self.layer))[0]
+
+
+def sequential_to_scan(model):
+    def fn(seq):
+        layers = seq.sublayers
+        layers = [l for l in layers if not isinstance(l, pz.nn.Identity)]
+        layer = jax.tree_map(lambda *xs: pz.nx.stack(xs, "layer"), *layers)
+        return ScanSequential(layer)
+
+    return model.select().at_instances_of(pz.nn.Sequential).pick_nth_selected(1).apply(fn)
