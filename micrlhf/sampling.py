45adfdfb2a40cd3ff0fedb33ec2a7f3f5500b154
nev <sshabalin3@gatech.edu>
2024-05-14 09:12:11 +0000

    More activation engineering, sampling improvements

--- micrlhf/sampling.py
+++ micrlhf/sampling.py
@@ -1,6 +1,7 @@
 import dataclasses
 from collections import OrderedDict
 from functools import partial
+from typing import Union, Tuple
 
 import jax
 import jax.numpy as jnp
@@ -11,28 +12,40 @@ from penzai.toolshed import jit_wrapper
 from tqdm.auto import trange
 
 from micrlhf.caching_llama import (LlamaKVCachingInputs,
-                                   LlamaKVCachingTransformer)
+                                   LlamaKVCachingTransformer,
+                                   LlamaKVCachingState)
 from micrlhf.llama import LlamaTransformer
 from micrlhf.tokenizer import load_tokenizer
 
 
-def sample(llama: LlamaTransformer, tokenizer : tiktoken.Encoding | transformers.PreTrainedTokenizerBase,
+def sample(llama: Union[LlamaTransformer, Tuple[LlamaKVCachingTransformer, LlamaKVCachingState]],
+           tokenizer : tiktoken.Encoding | transformers.PreTrainedTokenizerBase,
             # TODO: multiple prompts and left padding
            prompt: str,
            batch_size: int = 4,
            max_seq_len: int = 64,
-           pad_token_id: int = 128_020):
+           pad_token_id: int = 128_020,
+           do_sample: bool = False,
+           return_model: bool = False):
     tokens = tokenizer.encode(prompt)
     initial_length = len(tokens)
     tokens = [tokens + [pad_token_id] * (max_seq_len - len(tokens))]
     tokens = jnp.asarray(tokens, dtype=jnp.int32)
     tokens = pz.nx.NamedArray(OrderedDict(batch=1, seq=max_seq_len), tokens)
     tokens = tokens.untag("batch").repeat(batch_size).tag("batch")
-    llama_cached, cache = LlamaKVCachingTransformer.from_uncached(llama, max_seq_len, {"batch": batch_size})
-    llama_cached = jit_wrapper.Jitted(llama_cached)
+    if isinstance(llama, tuple):
+        llama_cached, cache = llama
+    else:
+        llama_cached, cache = LlamaKVCachingTransformer.from_uncached(llama, max_seq_len, {"batch": batch_size})
+        llama_cached = jit_wrapper.Jitted(llama_cached)
 
     def sample(logits, tokens, cache, key):
-        choices = logits.untag("vocabulary").argmax().untag("seq")[cache.cache_end_index - 1]
+        if not do_sample:
+            key_sample, key = jax.random.split(key)
+            choices = pz.nx.nmap(lambda l: jax.random.categorical(key_sample, l))(
+                logits.untag("batch", "vocabulary")).tag("batch").untag("seq")[cache.cache_end_index - 1]
+        else:
+            choices = logits.untag("vocabulary").argmax().untag("seq")[cache.cache_end_index - 1]
         tokens = pz.nx.nmap(lambda t, c: t.at[cache.cache_end_index].set(c))(tokens.untag("seq"), choices).tag("seq")
         return choices, tokens, key
 
@@ -54,7 +67,7 @@ def sample(llama: LlamaTransformer, tokenizer : tiktoken.Encoding | transformers
         inputs = LlamaKVCachingInputs(
             tokens=advanced[None].tag("seq"),
             positions=pz.nx.full({"batch": batch_size, "seq": 1}, cache.cache_end_index, jnp.int32),
-            attention_mask=((cache.cache_end_index >= pz.nx.arange("kv_seq", max_seq_len, dtype=jnp.int32))
+            attention_mask=((pz.nx.wrap(cache.cache_end_index) >= pz.nx.arange("kv_seq", max_seq_len, dtype=jnp.int32))
                             & (base_mask | (pz.nx.arange("seq", max_seq_len, dtype=jnp.int32) >= initial_length)
                                ).untag("seq").tag("kv_seq"))[None].tag("seq"),
             sampling_state=cache
@@ -67,7 +80,11 @@ def sample(llama: LlamaTransformer, tokenizer : tiktoken.Encoding | transformers
        advanced, tokens, cache, key = sample_step(llama_cached, advanced, tokens, cache, key)
         # bar.set_description(tokenizer.decode(tokens.untag("batch", "seq").data_array[0]))
 
-    return [tokenizer.decode(sequence) for sequence in tokens.untag("batch", "seq").data_array]
+    texts = [tokenizer.decode(sequence) for sequence in tokens.untag("batch", "seq").data_array]
+    if return_model:
+        return texts, (llama_cached, cache)
+    else:
+        return texts
 
 
 def main(
