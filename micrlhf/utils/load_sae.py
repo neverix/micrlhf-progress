8c589a85c0dd9e3db795d805a0133a7a75a981df
nev <sshabalin3@gatech.edu>
2024-05-23 15:41:51 +0000

    Save SAE util

--- micrlhf/utils/load_sae.py
+++ micrlhf/utils/load_sae.py
@@ -5,7 +5,7 @@ from safetensors.flax import load_file
 import jax
 
 sae_cache = {}
-def get_sae(layer=20, revision=5, idx=0):
+def get_sae(layer=20, revision=5, idx=0, model_dir="models", return_fname=False):
     key = (layer, revision)
     if key in sae_cache:
         return sae_cache[key]
@@ -14,9 +14,11 @@ def get_sae(layer=20, revision=5, idx=0):
     weight = sorted(weights)[idx]
     sparsity = float("-".join(weight.split("/")[2].split("-")[4:]))
     os.makedirs("models/sae", exist_ok=True)
-    fname = f"models/sae/{layer}-{revision}-{sparsity}.safetensors"
+    fname = f"{model_dir}/sae/{layer}-{revision}-{sparsity}.safetensors"
     w = "/".join(weight.split("/")[2:])
     os.system(f'wget -c "https://huggingface.co/nev/phi-3-4k-saex-test/resolve/main/{w}?download=true" -O "{fname}"')
+    if return_fname:
+        return fname
     sae_weights = load_file(fname)
     sae_cache[key] = sae_weights
     return sae_weights
