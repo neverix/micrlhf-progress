2c1f09a32a82620073ec532ca6818ef91059886a
nev <sshabalin3@gatech.edu>
2024-08-15 22:38:25 +0000

    Incorrect answer detection

--- micrlhf/utils/mmlu_eval.py
+++ micrlhf/utils/mmlu_eval.py
@@ -6,6 +6,10 @@ from more_itertools import chunked
 from penzai import pz
 from tqdm.auto import tqdm
 
+from micrlhf.llama import LlamaBlock
+from micrlhf.flash import flashify
+from micrlhf.sampling import sample, trange, jnp, load_tokenizer, jit_wrapper
+
 from micrlhf.sampling import jit_wrapper, jnp
 from micrlhf.scan import sequential_to_scan
 
@@ -31,6 +35,7 @@ Answer: ({}""")
 class MMLUEval(object):
     def __init__(self, dataset_path: str = "data/mmlu.tar", prompt_format="phi"):
         dataset = []
+        incorrect_dataset = []
         with tarfile.open(dataset_path) as data:
             for m in data.getmembers():
                 if not m.name.startswith("data/val"):
@@ -39,8 +44,12 @@ class MMLUEval(object):
                     continue
                 df = pd.read_csv(data.extractfile(m))
                 for _, r in df.iterrows():
-                    dataset.append(combined_prompts[prompt_format].format(*r.tolist()))
+                    r = r.tolist()
+                    dataset.append(combined_prompts[prompt_format].format(*r))
+                    for other_letter in (set("ABCD") - {r[-1]}):
+                        incorrect_dataset.append(combined_prompts[prompt_format].format(*r[:-1], other_letter))
         self.dataset = dataset
+        self.incorrect_dataset = incorrect_dataset
 
     def evaluate(self, llama, tokenizer, batch_size=128, verbose=True):
         ps = tokenizer.padding_side
@@ -73,3 +82,47 @@ class MMLUEval(object):
             accuracies.extend(probs.tolist())
         tokenizer.padding_side = ps
         return sum(accuracies) / len(accuracies)
+
+    def get_resids(self, llama, tokenizer, layer, batch_size=128, verbose=True):
+        ps = tokenizer.padding_side
+        tokenizer.padding_side = "right"
+
+        get_resids = llama.select().at_instances_of(LlamaBlock).apply_with_selected_index(lambda i, x:
+            pz.nn.Sequential([
+                pz.de.TellIntermediate.from_config(tag=f"resid_pre_{i}"),
+                x
+            ])
+        )
+        get_resids = pz.de.CollectingSideOutputs.handling(get_resids, tag_predicate=lambda x: x.startswith("resid_pre"))
+
+        @jax.jit
+        def get_embeds(llama, get_resids, token_array, mask):
+            token_array = pz.nx.wrap(token_array, "batch", "seq").untag("batch").tag("batch")
+            inputs = llama.inputs.from_basic_segments(token_array)
+            _, resids = get_resids(inputs)
+            # mask = pz.nx.wrap(mask, "batch", "seq")
+            resid = resids[layer].value.unwrap("batch", "seq", "embedding")
+            return jax.vmap(lambda m, x: x[m.astype(jnp.int32).sum() - 1], in_axes=(0, 0), out_axes=0)(mask, resid)
+            # return pz.nx.nmap(lambda m, x: x[m.sum() - 1])(mask.untag("seq"), resid.untag("seq")).unwrap("batch", "embedding")
+
+        embeds = []
+        corrects = []
+        for correct in (False, True):
+            dataset = (self.incorrect_dataset, self.dataset)[int(correct)]
+            for batch in chunked((tqdm(dataset) if verbose else dataset), batch_size):
+                batch = batch + [""] * (batch_size - len(batch))
+                tokens = tokenizer.batch_encode_plus(batch,
+                                                    return_tensors="np",
+                                                    padding="max_length",
+                                                    truncation=True,
+                                                    max_length=256,
+                                                    return_attention_mask=True)
+                token_array = jnp.asarray(tokens["input_ids"])
+                token_array = jax.device_put(token_array, jax.sharding.NamedSharding(llama.mesh, jax.sharding.PartitionSpec("dp", "sp")))
+                mask = jnp.asarray(tokens["attention_mask"])
+                mask = jax.device_put(mask, jax.sharding.NamedSharding(llama.mesh, jax.sharding.PartitionSpec("dp", "sp")))
+                embed = get_embeds(llama, get_resids, token_array, mask)
+                embeds.extend(list(jax.device_put(embed, jax.devices("cpu")[0])))
+                corrects.extend([int(correct)] * len(batch))
+        tokenizer.padding_side = ps
+        return list(zip(embeds, corrects))
