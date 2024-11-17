7e7cc11d557f6563cd462701ebcf325312c0b467
kisate <dhkas@mail.ru>
2024-11-17 03:55:37 +0000

    arxiv version + rebuttals experiments

---
+++ sprint/run_tvc_tune.py
@@ -0,0 +1,29 @@
+import os
+from sprint.task_vector_utils import load_tasks
+
+# Load tasks
+tasks = load_tasks()
+
+
+task_names = list(tasks.keys())
+
+import subprocess
+
+from tqdm.auto import tqdm
+
+n_parts = 4
+
+batched_task_names = [
+    task_names[i * n_parts : (i + 1) * n_parts] for i in range(len(task_names) // n_parts + 1)
+]
+
+
+for batch in tqdm(batched_task_names):
+    subprocess.run(
+        [
+            "python",
+            "sprint/tvc_tune.py",
+            ",".join(batch),
+            'g',
+        ]
+    )
